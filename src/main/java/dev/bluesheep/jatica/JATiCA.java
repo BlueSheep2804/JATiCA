f2febdc255e4aa4f5b67e507e9d8d6e8aced93bd
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-10-22 19:19:32 +0900

    first commit

---
+++ src/main/java/dev/bluesheep/jatica/JATiCA.java
@@ -0,0 +1,13 @@
+package dev.bluesheep.jatica;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(JATiCA.MODID)
+public class JATiCA {
+    public static final String MODID = "jatica";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public JATiCA() {}
+}
